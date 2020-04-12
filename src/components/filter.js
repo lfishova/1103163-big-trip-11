ef1edda1927afb793ee090eeb51aa3ab0c186377
Lena Fishova <matthewfisher1411@gmail.com>
2020-04-12 11:50:38 +0300

    Отрефакторит создание фильтров. Добавит параметр `isChecked`

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,14 +1,15 @@
-const createFilterMarkup = (name) => {
+const createFilterMarkup = (filter, isChecked) => {
+  const {name} = filter;
   return (
     ` <div class="trip-filters__filter">
-        <input id="filter-${name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name}" checked>
+        <input id="filter-${name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name}" ${isChecked ? `checked` : ``}>
         <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
     </div> `
   );
 };
 
 export const createFilterTemplate = (filters) => {
-  const filterMarkup = filters.map((it) => createFilterMarkup(it.name)).join(`\n`);
+  const filterMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
   return (
     ` <form class="trip-filters" action="#" method="get">
         ${filterMarkup}
